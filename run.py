8ba684db34b59f30b7d3b2dc42c98dd7220e8a8c
minow3 <minow3@gmail.com>
2023-01-22 13:52:03 +0000

    style: wellcome message and user menu

--- run.py
+++ run.py
@@ -6,25 +6,26 @@ print('------------Wellcome To------------')
 print("-----------------------------------")
 print("-----------SLOT MACHINE------------")
 print("-----------------------------------")
-print("Starting 1000 money added")
-print("One spin cost 10")
+print("Starting 1000 money added...")
+print("-----------------------------------")
+print("     SPIN 10     |     WIN 1000    ")
 print("-----------------------------------")
 
 while True: #main game function
-    DESITION = str(input("Type [S] to Spin [E] to Exit :"))
-    num1 = random.randint(1,5) #num1, num2, num3 numbers can be changed 0-9 for harder difficulity
+    DESITION = str(input(" Type [S] Spin   |   [E] to Exit :"))
+    num1 = random.randint(1,5) #(1,5)  >>> (0,9) for harder difficulity
     num2 = random.randint(1,5)
     num3 = random.randint(1,5)
-    if DESITION == "S":
+    if DESITION == "S": # start game
         print("-----------------------------------")
-        print(f"\t\t{num1}|{num2}|{num3}")
+        print(f"\t\t{num1}|{num2}|{num3}") # align to center
         print("-----------------------------------")
         if num1 == num2 == num3:
             print("you won 1000")
         else:
             print("you lost 10")
-    elif DESITION == "E":
-        print("Thank you for playing. Game closing...")
+    elif DESITION == "E": # exit the game
+        print("Thanks for playing. Game closing...")
         break
     else:
         print("wrong input")
