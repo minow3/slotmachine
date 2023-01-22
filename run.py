1ce2163dd32160e0a85d48146183b24563a52add
minow3 <minow3@gmail.com>
2023-01-22 16:44:02 +0000

    fix: typo

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ ACCOUNT = 1000 # starting money + update after effect
 WINS = 0 #counting wins
 
 print("-----------------------------------")
-print("      Wellcome to slot machine     ")
+print("      Welcome to slot machine      ")
 print("      1000 added to account...     ")
 print("-----------------------------------")
 print("-----------------------------------")
@@ -25,12 +25,12 @@ print("-----------------------------------")
 
 
 while True: #main game function
-    print("  [S] to Spin   |   [E] to Exit")
+    print("  [s] to Spin   |   [e] to Exit")
     DESITION = str(input("-----------------------------------\n   Type :"))
     num1 = random.randint(1,5) #(1,5)  >>> (0,9) for harder difficulity
     num2 = random.randint(1,5) # change all nums
     num3 = random.randint(1,5) # to get harder difficulty
-    if DESITION == "S": # start game
+    if DESITION == "s": # start game
         os.system('cls') # ('cls') windows, ('clear') linux/mac
         print("-----------------------------------")
         print("-----------------------------------")
@@ -56,13 +56,13 @@ while True: #main game function
             print("-----------------------------------")
             print("           You lost 10             ")
             print("-----------------------------------")
-    elif DESITION == "E": # exit the game
+    elif DESITION == "e": # exit the game
         print("-----------------------------------")
         print("Thanks for playing. Game closing...")
         print("-----------------------------------")
         break
     else:
-        os.system('cls')
+        os.system('cls') # ('cls') windows, ('clear') linux/mac
         print("-----------------------------------")
         print("           Wrong input             ")
         print("-----------------------------------")
